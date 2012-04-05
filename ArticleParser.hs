86c6ac0e4e7005e0c207fa11b4dbc2a629a74086
Yusuke Nomura <yunomu@gmail.com>
2012-04-06 08:09:38 +0900

    記事本文をStringとして取得

--- ArticleParser.hs
+++ ArticleParser.hs
@@ -1,29 +1,34 @@
 module ArticleParser (Article, getArticle) where
 
 import Text.HTML.TagSoup
-import Text.StringLike (StringLike, toString)
+import Text.HTML.TagSoup.Tree
+import Text.StringLike (StringLike, toString, fromString)
 
 data Article = Article {
     articleId :: String,
     title :: String,
     link :: String,
-    date :: String}
+    date :: String,
+    body :: String}
 
 instance Show Article where
     show a = "A {id = " ++ articleId a
       ++ ", title = " ++ title a
       ++ ", link = " ++ link a
-      ++ ", date = \"" ++ date a ++ "\"}"
+      ++ ", date = \"" ++ date a
+      ++ ", body = \"" ++ body a ++ "\"}"
 
 getArticle :: String -> String -> Either String Article
 getArticle atitle content = do
     aid <- getId tags
     adate <- getDate tags
+    abody <- getBody tags
     return Article {
       articleId = aid,
       title = atitle,
       link = "http://dic.nicovideo.jp/id/" ++ aid,
-      date = adate}
+      date = adate,
+      body = abody}
   where
     tags = parseTags content
 
@@ -68,10 +73,41 @@ getDate ts = getTagText "span" (tos f) ts
   where
     f n v = n == "style" && v == "color:red;"
 
+getBody :: (StringLike str, Show str) =>
+    [Tag str] -> Either String String
+getBody tags = Right $ plane $ search "div" "article" $ tagTree tags
+
+plane :: (StringLike str) => [TagTree str] -> String
+plane tree = plane' "" $ flattenTree tree
+  where
+    plane' :: StringLike str => String -> [Tag str] -> String
+    plane' r []                 = r
+    plane' r ((TagText str):ts) = plane' (r ++ toString str) ts
+    plane' r (_:ts)             = plane' r ts
+
+search :: (StringLike str) =>
+    String -> String -> [TagTree str] -> [TagTree str]
+search tag attrid trees = search' trees
+  where
+    search' :: StringLike str =>
+        [TagTree str] -> [TagTree str]
+    search' [] = []
+    search' ((TagBranch tagname as children):ts)
+      | toString tagname == tag
+        && matchAttrId as attrid = children
+      | otherwise                = search' (children ++ ts)
+    search' (_:ts) = search' ts
+
+    matchAttrId :: StringLike str => [Attribute str] -> String -> Bool
+    matchAttrId [] _ = False
+    matchAttrId ((n,v):as) aid
+      | toString n == "id" && toString v == aid = True
+      | otherwise = matchAttrId as aid
+
 {-
 main :: IO ()
 main = do
-    str <- readFile "test/error.html"
+    str <- readFile "test/ume.html"
     print $ getArticle "ume" str
 ---}
 
