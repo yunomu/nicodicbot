247a4e6d2a27211a242289ab626a2935d0b08984
Yusuke Nomura <yunomu@gmail.com>
2012-04-30 01:28:48 +0900

    parallel

--- Nicodicbot.hs
+++ Nicodicbot.hs
@@ -4,6 +4,7 @@ import System.IO
 import Codec.Binary.UTF8.String (encodeString, decodeString)
 import Network.Curl hiding (Content)
 import Data.Maybe
+import qualified Control.Monad.Parallel as P
 
 import Config
 import RssParser
@@ -11,18 +12,22 @@ import ArticleParser
 
 article :: Entry -> IO (Maybe Article)
 article entry = do
-    (CurlOK, content) <- curlGetString (rss_link entry) []
-    case getArticle (rss_title entry) (decodeString content) of
-      Left msg -> return Nothing
-      Right a  -> return $ Just a
+    (code, content) <- curlGetString (rss_link entry) []
+    case code of
+      CurlOK -> do
+        case getArticle (rss_title entry) (decodeString content) of
+          Left msg -> return Nothing
+          Right a  -> return $ Just a
+      other  -> return Nothing
 
 main :: IO ()
 main = do
     config <- loadConfig "nicodicbot.config"
     (CurlOK, rss) <- curlGetString (rssUri config) []
     let es = entries $ decodeString rss
-    articles <- mapM article $ es
+    articles <- P.mapM article $ es
     let as = catMaybes articles
     let keys = keyWord config
-    mapM_ print $ filter (strContain keys) as
+    P.mapM print $ filter (strContain keys) as
+    return ()
 
