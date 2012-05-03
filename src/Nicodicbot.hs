50399ede104bf23d723f6990e6a2b065a970f4bc
Yusuke Nomura <yunomu@gmail.com>
2012-05-03 13:02:53 +0900

    rewrite config parser

--- src/Nicodicbot.hs
+++ src/Nicodicbot.hs
@@ -4,6 +4,7 @@ import System.IO
 import Codec.Binary.UTF8.String (decodeString)
 import Network.Curl
 import Data.Maybe
+import Data.Functor
 import qualified Control.Monad.Parallel as P
 
 import Config
@@ -27,19 +28,21 @@ main :: IO ()
 main = do
     hSetEncoding stdout utf8
     config <- loadConfig "nicodicbot.config"
-    (code, rss) <- curlGetString (cfg_rssuri config) []
-    curlOK code (fail "Couldn't get rss") $ return ()
-    let es = entries $ decodeString rss
-    let keys = cfg_keywords config
-    articles <- P.mapM (f keys) es
---    mapM_ print $ catMaybes articles
-    mapM_ (putStrLn . dump) $ catMaybes articles
+    let uri = cfg_rssuri config
+    (code, contents) <- curlGetString uri []
+    curlOK code (fail $ "Couldn't get rss: " ++ uri) $ return ()
+    let rss = decodeString contents
+    let keywords = cfg_keywords config
+    articles <- fmap catMaybes $ P.mapM (f keywords) $ entries rss
+--    mapM_ print articles
+    mapM_ (putStrLn . dump) articles
   where
-    f :: [String] -> Entry -> IO (Maybe Article)
-    f keys entry = do
-        ma <- article entry
-        return $ maybe Nothing (toMaybe $ strContain keys) ma
+    f :: [String] -> (Entry -> IO (Maybe Article))
+    f keys = \entry -> maybe
+        Nothing
+        (toMaybe $ strContain keys)
+        <$> article entry
 
-    toMaybe :: (a -> Bool) -> a -> Maybe a
-    toMaybe b a = if b a then Just a else Nothing
+    toMaybe :: (a -> Bool) -> (a -> Maybe a)
+    toMaybe g = \a -> if g a then Just a else Nothing
 
